87790b516d6e5aa6d2d807ce8a575346d27670d4
GET Academy <terje@kolderup.net>
2026-02-26 13:53:56 +0100

    Create LinqQuerySyntaxDemo.cs

---
+++ SchoolAPI/LinqQuerySyntaxDemo.cs
@@ -0,0 +1,31 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolAPI
+{
+    public class LinqQuerySyntaxDemo
+    {
+        public static void Run()
+        {
+            // SQL
+            // SELECT Id, Name, Age
+            // FROM Students
+            // WHERE Age >= 18
+            // ORDER BY Name;
+            var context = new ApiContext(null);
+
+            var query =
+                from s in context.Students
+                where s.Age >= 18
+                orderby s.Name
+                select new
+                {
+                    s.Id,
+                    s.Name,
+                    s.Age
+                };
+
+            var result = query.ToList();
+        }
+    }
+}
