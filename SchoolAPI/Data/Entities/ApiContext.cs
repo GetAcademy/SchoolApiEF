34cf1566dd51d2517b8a00a0946a220f1a171c2b
GET Academy <terje@kolderup.net>
2026-02-26 13:50:28 +0100

    x

---
+++ SchoolAPI/Data/Entities/ApiContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public partial class ApiContext : DbContext
+{
+    public ApiContext(DbContextOptions<ApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Student__3214EC0707345EC4");
+
+            entity.ToTable("Student");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ SchoolAPI/Data/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Age { get; set; }
+}
